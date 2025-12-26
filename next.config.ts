00350638e8906c2391b694fb1e17a9183a253b5f
Gustavo Caro <psi.gustavocaro@gmail.com>
2025-12-26 01:11:17 -0300

    fix: update CSP to allow Paddle production domains

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.paddle.com https://sandbox-buy.paddle.com;
-              style-src 'self' 'unsafe-inline' https://sandbox-cdn.paddle.com;
+              style-src 'self' 'unsafe-inline' https://cdn.paddle.com https://sandbox-cdn.paddle.com;
               img-src 'self' blob: data: https://*.paddle.com https://dojetjntlqidtfdtykxt.supabase.co https://neurometricslatam.com;
               font-src 'self';
               frame-src 'self' https://sandbox-buy.paddle.com https://buy.paddle.com;
